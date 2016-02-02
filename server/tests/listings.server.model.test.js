65f05fdcabf6ceef4619cfb02d955509cad1ee8d
Alejandro Pena <alejandropenacai@ufl.edu>
2016-02-01 20:35:27 -0500

    last edit to maps

--- server/tests/listings.server.model.test.js
+++ server/tests/listings.server.model.test.js
@@ -1,11 +1,11 @@
-var should = require('should'), 
-    mongoose = require('mongoose'), 
-    Listing = require('../models/listings.server.model'), 
+var should = require('should'),
+    mongoose = require('mongoose'),
+    Listing = require('../models/listings.server.model'),
     config = require('../config/config');
 
 var listing = {
   code: 'LBW',
-  name: 'Library West', 
+  name: 'Library West',
   address: '1545 W University Ave, Gainesville, FL 32603, United States'
 };
 
@@ -18,15 +18,15 @@ describe('Listing Schema Unit Tests', function() {
 
   describe('Saving to database', function() {
     /*
-      Mocha's default timeout for tests is 2000ms. Saving to MongoDB is an asynchronous task 
-      that may take longer thatn 2000ms. To ensure that the tests do not fail prematurely, 
+      Mocha's default timeout for tests is 2000ms. Saving to MongoDB is an asynchronous task
+      that may take longer thatn 2000ms. To ensure that the tests do not fail prematurely,
       we can increase the timeout setting with the method this.timeout()
      */
     this.timeout(10000);
 
     it('saves properly when code and name provided', function(done){
       new Listing({
-        name: listing.name, 
+        name: listing.name,
         code: listing.code
       }).save(function(err){
         should.not.exist(err);
